5ef4ed7de0ade0de70e4ab4665f62e24ec13e285
Stacey <stacey.sych@gmail.com>
2021-03-18 15:20:08 +0300

    Login: layout

--- src/components/users/Login/Login.tsx
+++ src/components/users/Login/Login.tsx
@@ -2,16 +2,25 @@ import React from "react";
 
 import "./Login.scss";
 
+import user from "images/user.svg";
+import padlock from "images/padlock.svg";
+
 const Login = () => (
   <form method="post" className="Login">
     <h2 className="Login__title">Sing In</h2>
     <div className="Login__content">
       <label htmlFor="login">
-        <b>Login:</b>
+        <span>
+          <img src={user} alt="user" />
+          Login:
+        </span>
         <input type="text" placeholder="Enter Login" name="login" required />
       </label>
       <label htmlFor="password">
-        <b>Password:</b>
+        <span>
+          <img src={padlock} alt="padlock" />
+          Password:
+        </span>
         <input type="text" placeholder="Enter Password" name="password" required />
       </label>
       <button type="submit" className="Login__btn">
