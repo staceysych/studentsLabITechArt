d08b797426030bd2ad6279b4d4574f6669607d23
Stacey <stacey.sych@gmail.com>
2021-04-13 15:13:16 +0300

    Select: use DropDownMenu and Item components

--- src/components/products/DropDownItem/DropDownItem.tsx
+++ src/components/products/DropDownItem/DropDownItem.tsx
@@ -4,15 +4,21 @@ import { NavLink } from "react-router-dom";
 interface Props {
   path?: string;
   name: string;
-  handleDropDownClick: () => void;
-  icon: any;
+  handleDropDownClick?: any;
+  icon?: any;
+  isSelect?: boolean;
 }
 
-const DropDownItem: React.FC<Props> = ({ path, name, handleDropDownClick, icon }) => (
-  <NavLink className="DropDownMenu__item" to={path} onClick={handleDropDownClick}>
-    <img className="DropDownMenu__icon" src={icon} alt={name} />
-    {name}
-  </NavLink>
-);
+const DropDownItem: React.FC<Props> = ({ path, name, handleDropDownClick, icon, isSelect }) =>
+  isSelect ? (
+    <li className="DropDownMenu__item" data-name={name} onClick={handleDropDownClick}>
+      {name}
+    </li>
+  ) : (
+    <NavLink className="DropDownMenu__item" to={path} onClick={handleDropDownClick}>
+      <img className="DropDownMenu__icon" src={icon} alt={name} />
+      {name}
+    </NavLink>
+  );
 
 export default DropDownItem;
